d6209ef78fcb1667ef90bbd020ce186affcc716c
Niktia Bykov <kilops.com@gmail.com>
2023-10-30 12:09:08 +0500

    aggregation done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from datetime import datetime
 
 client = MongoClient("localhost", 27017)
 
@@ -12,18 +12,61 @@ test_input = {
     "group_type": "month",
 }
 
-pipeline = [
-    {"$project": {"date_only": {"$substr": ["$dt", 0, 10]}, "value": "$value"}},
-    {"$group": {"_id": "$date_only", "sum": {"$sum": {"$toInt": "$value"}}}},
-    {"$sort": {"_id": 1}},
-]
+group_by_day_substr_params = [0, 10]
+group_by_month_susbtr_params = [0, 7]
+group_by_year_susbtr_params = [0, 3]
 
-date_list = []
-sum_list = []
 
-for entry in sample_collection.aggregate(pipeline):
-    date_list.append(entry["_id"])
-    sum_list.append(entry["sum"])
+def get_aggregated_payments(date_from: str, date_upto: str, group_type: str) -> dict:
+    substr_params = []
 
-print(date_list)
-print(sum_list)
+    match group_type:
+        case "day":
+            substr_params = group_by_day_substr_params
+        case "month":
+            substr_params = group_by_month_susbtr_params
+        case "year":
+            substr_params = group_by_year_susbtr_params
+        case _:
+            raise Exception
+
+    pipeline = [
+        {
+            "$match": {
+                "dt": {
+                    "$gte": datetime.fromisoformat(date_from),
+                    "$lt": datetime.fromisoformat(date_upto),
+                }
+            }
+        },
+        {
+            "$project": {
+                "date_only": {"$substr": ["$dt", *substr_params]},
+                "value": "$value",
+                "dt": "$dt",
+            }
+        },
+        {
+            "$group": {
+                "_id": "$date_only",
+                "sum": {"$sum": {"$toInt": "$value"}},
+            }
+        },
+        {"$sort": {"_id": 1}},
+    ]
+
+    date_list = []
+    sum_list = []
+
+    for entry in sample_collection.aggregate(pipeline):
+        date_list.append(entry["_id"])
+        sum_list.append(entry["sum"])
+
+    result = {"dataset": sum_list, "date_list": date_list}
+
+    return result
+
+
+test = get_aggregated_payments(
+    test_input["dt_from"], test_input["dt_upto"], test_input["group_type"]
+)
